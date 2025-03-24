bb95918775b5e9808cd89339c3955da69697e34b
Ryan Padden <rpadden97@gmail.com>
2025-03-24 09:24:16 +0000

    Implemented useProperties hook for PropertyListing to perform GET properties functionality

--- src/components/PropertyListing/PropertyListing.jsx
+++ src/components/PropertyListing/PropertyListing.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropertyCard from '../PropertyCard';
 import './PropertyListing.scss';
 
@@ -17,16 +17,66 @@ const DUMMY_PROPERTY = {
         'https://media.rightmove.co.uk/dir/crop/10:9-16:9/38k/37655/53588679/37655_CAM170036_IMG_01_0000_max_476x317.jpg',
 };
 
+/* 
+ * useProperties: hook for fetching property list from api
+ *  
+ * TODO(rpadden97):
+ * * Implement query parameter for filtering
+ * * Setup Loading state
+ * * Setup Error state
+*/
+const useProperties = () => {
+    const [propertyList, setPropertyList] = useState(Array(0))
+    const url = 'http://localhost:3000/api/properties'
+
+    useEffect(() => {
+        const fetchProperties = async () => {
+            try {
+                const response = await fetch(url)
+
+                if (!response.ok) {
+                  throw new Error(`Error status: ${response.status}`)
+                }
+            
+                const properties = await response.json()
+
+                setPropertyList(properties)
+            } catch (error) {
+                console.error(error.message)
+            }
+        }
+
+        fetchProperties()
+    }, [url])
+
+    return propertyList
+}
+
+/* 
+ * PropertyListing: renders list of properties
+ *  
+ * @component
+ * 
+ * TODO(rpadden97):
+ * * Add parameter for number of cards to be shown
+ * * Setup Loading screen -> Revist guard rail statement
+ * * Setup Error screen
+*/
 const PropertyListing = () => {
+    const properties = useProperties()
+
+    /* Discussion: Guard rails that cause tests to fail */
+    // if (properties.length == undefined || properties.length <= 0) { 
+    //     return (<div><p>Loading</p></div>)
+    // }
+
     return (
         <ul className="PropertyListing">
-            {Array(5)
-                .fill(DUMMY_PROPERTY)
-                .map((property, index) => (
-                    <li key={index}>
-                        <PropertyCard {...property} />
-                    </li>
-                ))}
+            {properties.slice(0,5).map((property, index) => (
+                <li key={index}>
+                    <PropertyCard {...property} />
+                </li>
+            ))}
         </ul>
     );
 };
